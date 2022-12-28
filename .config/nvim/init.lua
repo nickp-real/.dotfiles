247c5c8a9e949080e45b595e1b31d4613a4ebb3e
NickP-real <nick_narak@rocketmail.com>
2022-12-28 23:53:31 +0700

    same as before

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,13 +2,13 @@
 require("core.option")
 require("core.lazy")
 require("core.autocmd")
-require("util.auto_shebang")
+require("utils.auto_shebang")
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
   callback = function()
     require("core.mapping")
     require("core.user_cmd")
-    require("util.coderunner")
+    require("utils.coderunner")
   end,
 })
