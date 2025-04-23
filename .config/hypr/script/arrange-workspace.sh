9a66ed8e9603acc3d0df7c7ad6201def4554b56a
nickp-real <nick_narak@rocketmail.com>
2025-04-23 21:53:23 +0700

    refactor(hyprland): separate files per config group

--- .config/hypr/script/arrange-workspace.sh
+++ .config/hypr/script/arrange-workspace.sh
@@ -2,55 +2,36 @@
 max_monitor_support=2
 monitor_count=$(hyprctl monitors -j | jq '. | length')
 
-if [[ "$monitor_count" -gt "$max_monitor_support" ]]; then
+if [[ "$monitor_count" -gt "$max_monitor_support" || "$monitor_count" -eq 1 ]]; then
   echo "Not supported"
   exit
 fi
 
-# function bindws() {
-# 	hyprctl keyword workspace "$2", "$1" >/dev/null
-# 	hyprctl dispatch moveworkspacetomonitor "$1" "$2" >/dev/null
-# }
-
-# if [[ "$monitor_count" == 1 ]]; then
-# 	monitor_1=$(hyprctl monitors -j | jq '.[0].name')
-# 	for ((i = 1; i <= 10; i++)); do
-# 		bindws $i "$monitor_1"
-# 	done
-# fi
-
-# if [[ "$monitor_count" == 2 ]]; then
-# 	monitor_1=$(hyprctl monitors -j | jq '.[0]')
-# 	monitor_2=$(hyprctl monitors -j | jq '.[1]')
-#   monitor_1_x=$(jq ".x" <<< "$monitor_1")
-
-#   if [ "$monitor_1_x" == "0" ]; then
-# 		primary_monitor=$(jq ".name" <<<"$monitor_1")
-# 		secondary_monitor=$(jq ".name" <<<"$monitor_2")
-#   else
-# 		primary_monitor=$(jq ".name" <<<"$monitor_2")
-# 		secondary_monitor=$(jq ".name" <<<"$monitor_1")
-#   fi
-
-# 	for ((i = 1; i <= 5; i++)); do
-# 		bindws $i "$primary_monitor"
-# 	done
-
-# 	for ((i = 6; i <= 10; i++)); do
-# 		bindws $i "$secondary_monitor"
-# 	done
-# fi
+default_monitor="eDP-1"
 
 monitor=$(hyprctl monitors -j | jq '.[1].name' | tr -d '"')
+if [[ "$monitor" -eq "$default_monitor" ]]; then
+  monitor=$(hyprctl monitors -j | jq '.[0].name' | tr -d '"')
+fi
 
-if [[ "$monitor" == "DP-2" ]]; then
-  hyprctl keyword workspace 1,monitor:"$monitor",default:true >/dev/null
-  hyprctl keyword workspace 6,monitor:eDP-1,default:true >/dev/null
+bind_monitor() {
+  main=$1
+  sec=${2:-default_monitor}
+  hyprctl keyword workspace 1,monitor:"$main",default:true >/dev/null
+  hyprctl keyword workspace 6,monitor:"$sec",default:true >/dev/null
 
-  hyprctl dispatch moveworkspacetomonitor 1 "$monitor" >/dev/null
+  hyprctl dispatch moveworkspacetomonitor 1 "$main" >/dev/null
 
   hyprctl dispatch workspace 6 >/dev/null
-  hyprctl dispatch moveworkspacetomonitor 6 eDP-1 >/dev/null
+  hyprctl dispatch moveworkspacetomonitor 6 "$sec" >/dev/null
 
   hyprctl dispatch workspace 1 >/dev/null
+}
+
+echo "$monitor" "$default_monitor"
+
+if [[ "$monitor" == "DP-2" ]]; then
+  bind_monitor "$monitor"
+else
+  bind_monitor "$default_monitor" "$monitor"
 fi
