f4aa1b470cc6f7eb3c947ecbc385e58501418873
nickp-real <nick_narak@rocketmail.com>
2025-04-23 21:54:32 +0700

    perf(fish): source fish filetype for gcloud cli

--- .config/fish/conf.d/gcloud.fish
+++ .config/fish/conf.d/gcloud.fish
@@ -1,4 +1,5 @@
 if status --is-interactive
     # gcloud cli
-    bass source /etc/profile.d/google-cloud-cli.sh
+    # bass source /etc/profile.d/google-cloud-cli.sh
+    source /opt/google-cloud-cli/path.fish.inc
 end
