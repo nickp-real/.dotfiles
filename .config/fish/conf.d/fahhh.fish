81249dbfa29f0b07d892d031e563cfa8f8eca50a
nickp-real <nick_narak@rocketmail.com>
2026-03-04 12:56:43 +0700

    fix(fish): use absolute path for assets

--- .config/fish/conf.d/fahhh.fish
+++ .config/fish/conf.d/fahhh.fish
@@ -1,6 +1,6 @@
 function fahhh_handler --on-event fish_postexec
     if test ! $status -eq 0
         and status --is-interactive
-        pw-play ../assets/fahhh-sound-effect.mp3 &
+        pw-play ~/.config/fish/assets/fahhh-sound-effect.mp3 &
     end
 end
