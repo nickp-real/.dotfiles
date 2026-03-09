8c2c8cb40e76f9a77662880d6750fed3d6f95c1f
nickp-real <nick_narak@rocketmail.com>
2026-03-09 18:44:01 +0700

    fix(hypr): new layoutmsg dispatcher

--- .config/hypr/scripts/gtktheme.sh
+++ .config/hypr/scripts/gtktheme.sh
@@ -2,16 +2,18 @@
 
 theme='OneDark'
 icon='Papirus-Dark'
-font='Roboto 12'
+font='Roboto 10'
 cursor='Bibata-Modern-Ice'
+color_scheme='prefer-dark'
 
 schema='gsettings set org.gnome.desktop.interface'
 
-apply_themes(){
+apply_themes() {
   ${schema} gtk-theme "${theme}"
   ${schema} icon-theme "${icon}"
   ${schema} cursor-theme "${cursor}"
   ${schema} font-name "${font}"
+  ${schema} color-scheme "${color_scheme}"
 }
 
 apply_themes
