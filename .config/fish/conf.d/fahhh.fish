677cb920f56ff3592b9f5d581ca6fac869d16c40
nickp-real <nick_narak@rocketmail.com>
2026-03-12 20:39:41 +0700

    fix(fish): decrease fahhh sound

--- .config/fish/conf.d/fahhh.fish
+++ .config/fish/conf.d/fahhh.fish
@@ -1,6 +1,6 @@
 function fahhh_handler --on-event fish_postexec
     if test ! $status -eq 0
         and status --is-interactive
-        pw-play ~/.config/fish/assets/fahhh-sound-effect.mp3 &
+        pw-play ~/.config/fish/assets/fahhh-sound-effect.mp3 --volume 0.1 &
     end
 end
